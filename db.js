6fa8580cc07efc00f914958a4323f307977e9c2c
Heber <heber.bar@gmail.com>
2023-11-14 18:32:50 +0100

    actualizacion password env

--- db.js
+++ db.js
@@ -1,5 +1,3 @@
-require("dotenv").config();
-
 const postgres = require("postgres");
 
 function conectar() {
--- index.js
+++ index.js
@@ -1,7 +1,32 @@
 require("dotenv").config();
 
 const express = require("express");
+const { leerTareas, crearTarea, editarTextoTarea, editarEstadoTarea, borrarTarea } = require("./db");
 
 const servidor = express();
+
+servidor.use("/pruebas_api", express.static("./pruebas_api"));
+
+servidor.get("/tareas", (peticion, respuesta) => {
+  leerTareas().then(([error, tareas]) => {
+    throw "hola";
+  });
+
+  // let [error, tareas] = await leerTareas();
+  // if (!error) {
+  //   return respuesta.json(tareas);
+  // }
+  // throw error;
+});
+
+servidor.use((error, peticion, respuesta, siguiente) => {
+  // cualquier excepcion que envie el sistema(throw ) sera capturado por este middleware
+  respuesta.send("...ocurrio un error");
+});
+
+servidor.use((peticion, respuesta) => {
+  respuesta.status(404);
+  respuesta.json({ error: "recurso no encontrado" });
+});
 
 servidor.listen(process.env.PORT);
